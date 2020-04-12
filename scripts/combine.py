b95376cfd84eb3044f1a62d6390095c0ffeef3d6
Justin Tinker <justin.tinker1@gmail.com>
2020-04-12 18:46:50 -0400

    script comments

--- scripts/combine.py
+++ scripts/combine.py
@@ -1,33 +1,47 @@
+'''
+Combines all of the .txt file in directory and subdirectories into one .txt file for group analysis
+'''
+
 import sys
 import os
 import io
 import re
 from sys import argv, exit
 
-def combine(path):
+def combine(path, delete_pdf = False):
+    '''
+    Recusively grabs text from all .txt files in path and its subdirectories
+
+    @param path: must be a directory
+    @param delete_pdf: if true, deletes all .pdf files in path and its subdirectories
+    '''
+
     combined_text = ''
     print(f"Path: {path}")
+    #slightly faster than os.listdir
     for filename in os.scandir(path):
+        #seperate files by newline
         combined_text += '\n'
         if filename.is_dir():
+            #recurse
             combined_text += combine(filename.path)
         elif filename.path.endswith('.txt'):
             print(f"processing file: {filename.path}")
             with open(filename.path) as f:
                 combined_text += f.read()
-        elif filename.path.lower().endswith('.pdf'):
+        elif delete_pdf and ilename.path.lower().endswith('.pdf'):
             os.remove(filename.path)
     return combined_text
 
+if __name__ == '__main__':
+    if len(argv) < 2:
+        print("usage combine.py [ROOT FOLDER] [combined_filename (optional)]")
+        print("ex. combine.py ./articles/2018/byrace")
+        print("ex. combine.py ./articles/2018/byrace all.txt")
 
-if len(argv) < 2:
-    print("usage combine.py [ROOT FOLDER] [combined_filename (optional)]")
-    print("ex. combine.py ./articles/2018/byrace")
-    print("ex. combine.py ./articles/2018/byrace all.txt")
+    root_folder = argv[1]
+    postfix = argv[2] if len(argv) > 2 else 'combined.txt'
 
-root_folder = argv[1]
-postfix = argv[2] if len(argv) > 2 else 'combined.txt'
-
-combined_text = combine(root_folder)
-with open(os.path.join(root_folder, postfix), 'w+') as f:
-    f.write(combined_text)
+    combined_text = combine(root_folder)
+    with open(os.path.join(root_folder, postfix), 'w+') as f:
+        f.write(combined_text)
--- scripts/textify.py
+++ scripts/textify.py
@@ -12,18 +12,13 @@ from pdfminer.pdfpage import PDFPage
 
 import enchant
 
-if len(argv) != 2:
-    print("usage: textify.py [PATH TO FOLDER]")
-    print("ex. textify.py ./articles/2018/byrace/B")
-
-english = enchant.Dict("en_US")
-
-#foldername = sys.argv[1]
-filelist = []
-
-
 
 def convert_pdf_to_txt(path):
+    '''
+    Effectively runs pdf2txt.py with default artguments
+    See 'pdf2txt.py' in the pdfminer script directory for details
+    '''
+
     rsrcmgr = PDFResourceManager()
     retstr = io.StringIO()
     codec = 'utf-8'
@@ -44,9 +39,15 @@ def convert_pdf_to_txt(path):
 
 
 def guess_text(text):
+    '''
+    Tries to get rid of header/footer text from pdfs
+    TODO: optimize for more article styles
+    '''
+
     if not text:
         return None
 
+    #one known article style begins relavent text with FULL TEXT and starts footer at DETAILS
     ft,ds = text.find('FULL TEXT'), text.find('DETAILS')
     if ft != -1 and ds != -1 and ft < ds:
         return text[ft + len('FULL TEXT'):ds]
@@ -54,32 +55,56 @@ def guess_text(text):
     return text
 
 def fix_text(text):
+    '''
+    Common issue with PDF to text is words getting broken up by spaces.
+    Leverages enchant's spell check to try to identify and put back together these words
+    '''
+
     fixed_text = ''
     words = text.split(' ')
+    #reverse loop to account for changing length when words are combined
     for i in range(len(words) - 1, -1, -1):
         try:
+            #ex. T uesday -> Tuesday or y ellow -> yellow
             if i > 0 and not english.check(words[i]) and english.check(words[i-1] + words[i]):
                 words[i - 1] += words[i]
                 continue
         except Exception as e:
-            #error with enchant library, don't try to fix word
+            #enchant complains when you give it an empty string, and for a few other unknown reasons
+            #if enchant can't process the word, we won't try to combine it
             print(e)
 
         fixed_text = words[i] + ' ' + fixed_text
     return fixed_text
 
-for dirpath,_,filenames in os.walk(argv[1]):
-    for filename in filenames:
-        if filename.endswith('.pdf'):
-            filelist.append(os.path.join(dirpath,filename))
+if __name__ == '__main__':
+    if len(argv) != 2 or argv[1] in ['-h', '--help']:
+        print("usage: textify.py [PATH TO FOLDER]")
+        print("ex. textify.py ./articles/2018/byrace/B")
 
-for file in filelist:
-    print(f"Converting file: {file}")
+    base_path = argv[1]
 
-    for name_word in re.split(' |_', os.path.basename(os.path.dirname(file))):
-      english.add(name_word)
+    #note: may need to install enchant C libraries with brew install enchant
+    english = enchant.Dict("en_US")
 
-    text = guess_text(fix_text(convert_pdf_to_txt(file)))
+    #grab all PDF files in directory and subdirectories
+    filelist = []
+    for dirpath,_,filenames in os.walk(base_path):
+        for filename in filenames:
+            if filename.endswith('.pdf'):
+                filelist.append(os.path.join(dirpath,filename))
 
-    with open(file[:-3] + 'txt',"w+") as f:
-        f.write(text)
+    for file in filelist:
+        print(f"Converting file: {file}")
+
+        #in Ryzdik's naming format, the foldername is the same as the victim's name
+        #we want victim's first/last/middle name to pass spell check
+        for name_word in re.split(' |_', os.path.basename(os.path.dirname(file))):
+          english.add(name_word)
+
+        #grab text with a few optimizations (combining words & locating start/end of full text)
+        text = guess_text(fix_text(convert_pdf_to_txt(file)))
+
+        #write to text file with same name
+        with open(file[:-3] + 'txt',"w+") as f:
+            f.write(text)
